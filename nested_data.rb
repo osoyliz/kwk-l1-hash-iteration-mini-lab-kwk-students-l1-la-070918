7dee0046e48cb8025fd8114ff406b6ae7561e836
osoyliz <lizosoy18@gmail.com>
2018-07-11 23:16:35 +0000

    automatically backed up by learn

---
+++ nested_data.rb
@@ -0,0 +1,39 @@
+# # nested arrays format 1 
+# clothes = [ 
+#   ["blouses", "t-shirts", "jackets"],
+#   ["jeans", "skirts", "shorts"],
+#   ["sandals", "sneakers", "heels"]
+#   ]
+# # nested array format 2  
+# tops = ["blouses", "t-shirts", "jackets"]
+# bottoms = ["jeans", "skirts", "shorts"]
+# footwear = ["sandals", "sneakers", "heels"]
+
+# clothes = [tops, bottoms, footwear]
+
+# #two different ways to add to array 
+# footwear.push("birkenstocks")
+# footwear << "uggs"
+
+# puts clothes [2][2]
+
+# closet= {
+#   "tops"=> ["rain jacket", "cardigan", "blazer"],
+#   "bottoms" => ["jeans", "skirt", "capris"],
+#   "footwear"=> ["sneakers", "heels","flats"]
+# }
+# puts closet ["tops"] 
+
+
+
+
+# baseball_players = {:babe_ruth => {:hits => 2873, :home_runs => 714, :obp => 474},
+# :barry_bonds => {:hits => 2935, :home_runs => 762, :obp => 444}}
+# puts baseball_players {
+
+people = [["Maddy" + " is a"+ " cool gal"],["Hunter"+  " is a " + " snorer"]]
+puts people
+
+people = [[" Maddy "," cool gal "],[" Hunter "," snorer "]]
+puts people
+
